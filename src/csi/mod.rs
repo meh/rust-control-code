a6b9fed6f0a0d09a0ce15fd35ffdad45c81ded9b
meh <meh@schizofreni.co>
2016-10-09 23:53:42 +0200

    *: fix license headers

--- src/csi/mod.rs
+++ src/csi/mod.rs
@@ -1,19 +1,16 @@
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
 // Copyleft (ↄ) meh. <meh@schizofreni.co> | http://meh.schizofreni.co
 //
-// This file is part of cancer.
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
 //
-// cancer is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 //
-// cancer is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cancer.  If not, see <http://www.gnu.org/licenses/>.
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 use std::str;
 use std::u32;
